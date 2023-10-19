4eeead8af9a9b9fb460ca0d431c4d414655895c8
sutoscience <amy@amysuto.com>
2023-10-19 12:02:52 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Purpose: To handle the login popup
+const loginButton = document.getElementById('loginButton');
+const loginPopup = document.getElementById('loginPopup');
+
+loginButton.addEventListener('click', function(event) {
+    event.preventDefault(); // prevent the default action (page navigation)
+    
+    if (loginPopup.style.display === 'none' || loginPopup.style.display === '') {
+        loginPopup.style.display = 'flex';
+    } else {
+        loginPopup.style.display = 'none';
+    }
+});
+
+function submitFunction() {
+    alert('Button has been clicked!');
+    // Place any other function or logic you want to execute here
+}
