39088ba9d034e1b5a3cf662cd4d56452a2b2e166
sutoscience <amy@amysuto.com>
2023-10-27 15:02:29 -0600

    gross 3:02pm

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 // Purpose: To handle the login popup
 const loginButton = document.getElementById('loginButton');
-const loginPopup = document.getElementById('loginPopup');
+const loginContainer = document.getElementById('login-container');
 
-loginButton.addEventListener('click', function(event) {
-    event.preventDefault(); // prevent the default action (page navigation)
-    
-    if (loginPopup.style.display === 'none' || loginPopup.style.display === '') {
-        loginPopup.style.display = 'block';
+loginButton.addEventListener('click', function() {
+    if (loginContainer.style.display === 'none' || loginContainer.style.display === '') {
+        loginContainer.style.display = 'block';
     } else {
-        loginPopup.style.display = 'none';
+        loginContainer.style.display = 'none';
     }
 });
 
