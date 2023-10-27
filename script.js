88b0ac4b9884c3a9e82250e210d6eff1b2d8e3f3
sutoscience <amy@amysuto.com>
2023-10-27 17:07:43 -0600

    weeee 5:07p

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 // Purpose: To handle the login popup
 const loginButton = document.getElementById('loginButton');
 const loginContainer = document.getElementById('login-container');
-
+const overlay = document.getElementById('overlay');
 
 document.addEventListener('click', function(event) {
-    var loginContainer = document.getElementById('login-container');
-    var loginButton = document.getElementById('loginButton');
+    var isClickInside = loginContainer.contains(event.target) || event.target === loginButton;
 
     // Check if the click is outside the login popup and not on the login button
-    if (!loginContainer.contains(event.target) && event.target !== loginButton) {
+    if (!isClickInside && loginContainer.style.display === 'block') {
         loginContainer.style.display = 'none';
+        overlay.style.display = 'none'; // Hide overlay
     }
 });
 
 loginButton.addEventListener('click', function() {
     if (loginContainer.style.display === 'none' || loginContainer.style.display === '') {
         loginContainer.style.display = 'block';
+        overlay.style.display = 'block'; // Show overlay
     } else {
         loginContainer.style.display = 'none';
+        overlay.style.display = 'none'; // Hide overlay
     }
+});
+
+overlay.addEventListener('click', function() {
+    loginContainer.style.display = 'none';
+    overlay.style.display = 'none'; // Hide overlay when it is clicked
 });
 
 function submitFunction() {
