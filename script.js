e9a30cddb498390f45eca926c112ba9bb4191266
sutoscience <amy@amysuto.com>
2023-10-28 12:11:21 -0600

    welll 12:11pm 10/28/23

--- script.js
+++ script.js
@@ -28,26 +28,11 @@ overlay.addEventListener('click', function() {
     overlay.style.display = 'none'; // Hide overlay when it is clicked
 });
 
-function submitFunction() {
-    alert('Button has been clicked!');
-    // Place any other function or logic you want to execute here
-}
-
-function toggleLoginPopup() {
-    var popup = document.getElementById('login-container');
-    if (popup.style.display === 'none' || popup.style.display === '') {
-        popup.style.display = 'block';
-    } else {
-        popup.style.display = 'none';
+document.getElementById('navbarId').addEventListener('click', function(event) {
+    if (event.target !== loginButton) { // Check if the clicked element is not the login button
+        loginContainer.style.display = 'none';
+        overlay.style.display = 'none';
     }
-}
-
-document.getElementById('navbarId').addEventListener('click', function() {
-    loginContainer.style.display = 'none';
-    overlay.style.display = 'none';
 });
 
-document.getElementById('logoId').addEventListener('click', function() {
-    loginContainer.style.display = 'none';
-    overlay.style.display = 'none';
-});
+// Other functions can go here
