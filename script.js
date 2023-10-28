3feaa69b9b7834b280f59ad2f80a4f2dea5ee566
sutoscience <amy@amysuto.com>
2023-10-28 12:32:20 -0600

    nope

--- script.js
+++ script.js
@@ -1,38 +1,24 @@
 window.onload = function() {
-    // Purpose: To handle the login popup
     const loginButton = document.getElementById('loginButton');
     const loginContainer = document.getElementById('login-container');
     const overlay = document.getElementById('overlay');
-    const navbarList = document.querySelector('#navbarId ul'); // Select the <ul> inside the navbar
+    const navbar = document.getElementById('navbarId');
 
     document.addEventListener('click', function(event) {
-        var isClickInside = loginContainer.contains(event.target) || event.target === loginButton;
+        var isClickInsideLogin = loginContainer.contains(event.target) || event.target === loginButton;
+        var isClickInsideNavbar = navbar.contains(event.target) && event.target !== loginButton;
 
-        // Check if the click is outside the login popup and not on the login button
-        if (!isClickInside && loginContainer.style.display === 'block') {
+        if (event.target === loginButton) {
+            if (loginContainer.style.display === 'none' || loginContainer.style.display === '') {
+                loginContainer.style.display = 'block';
+                overlay.style.display = 'block';
+            } else {
+                loginContainer.style.display = 'none';
+                overlay.style.display = 'none';
+            }
+        } else if (!isClickInsideLogin && !isClickInsideNavbar) {
             loginContainer.style.display = 'none';
-            overlay.style.display = 'none'; // Hide overlay
+            overlay.style.display = 'none';
         }
-    });
-
-    loginButton.addEventListener('click', function() {
-        if (loginContainer.style.display === 'none' || loginContainer.style.display === '') {
-            loginContainer.style.display = 'block';
-            overlay.style.display = 'block'; // Show overlay
-        } else {
-            loginContainer.style.display = 'none';
-            overlay.style.display = 'none'; // Hide overlay
-        }
-    });
-
-    overlay.addEventListener('click', function() {
-        loginContainer.style.display = 'none';
-        overlay.style.display = 'none'; // Hide overlay when it is clicked
-    });
-
-    navbarList.addEventListener('click', function() {
-        loginContainer.style.display = 'none';
-        overlay.style.display = 'none'; // Hide overlay when navbar list is clicked
     });
 }
-
