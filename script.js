5d79bb0bb049ae1a2839fad181a68ce5a62ef775
sutoscience <amy@amysuto.com>
2023-10-28 12:36:03 -0600

    stahp

--- script.js
+++ script.js
@@ -2,20 +2,31 @@ window.onload = function() {
     const loginButton = document.getElementById('loginButton');
     const loginContainer = document.getElementById('login-container');
     const overlay = document.getElementById('overlay');
-    const navbar = document.getElementById('navbarId');
+    const closeTriggers = document.querySelectorAll('.close-login');
 
     document.addEventListener('click', function(event) {
         if (event.target === loginButton) {
-            if (loginContainer.style.display === 'none' || loginContainer.style.display === '') {
-                loginContainer.style.display = 'block';
-                overlay.style.display = 'block';
-            } else {
-                loginContainer.style.display = 'none';
-                overlay.style.display = 'none';
-            }
-        } else if (!loginContainer.contains(event.target) && !navbar.contains(event.target)) {
-            loginContainer.style.display = 'none';
-            overlay.style.display = 'none';
+            toggleLoginPopup();
+        } else if (!loginContainer.contains(event.target)) {
+            closeLoginPopup();
         }
     });
+
+    closeTriggers.forEach(element => {
+        element.addEventListener('click', closeLoginPopup);
+    });
+
+    function toggleLoginPopup() {
+        if (loginContainer.style.display === 'none' || loginContainer.style.display === '') {
+            loginContainer.style.display = 'block';
+            overlay.style.display = 'block';
+        } else {
+            closeLoginPopup();
+        }
+    }
+
+    function closeLoginPopup() {
+        loginContainer.style.display = 'none';
+        overlay.style.display = 'none';
+    }
 }
