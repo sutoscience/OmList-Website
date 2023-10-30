a1203fce18636a3f0f651ef4b9e351d85b05f09d
sutoscience <amy@amysuto.com>
2023-10-30 15:15:53 -0600

    ya 3:15p

--- script.js
+++ script.js
@@ -115,3 +115,7 @@ function openCreateAccountPopup() {
 function closeCreateAccountPopup() {
     document.getElementById('create-account-popup').style.display = 'none';
 }
+
+document.getElementById("create-account-btn").addEventListener("click", function() {
+    document.getElementById("createAccountModal").style.display = "block";
+});
