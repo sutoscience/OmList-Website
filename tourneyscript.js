57315e59d0d5067f5717decf6a7e551b60397ef9
sutoscience <amy@amysuto.com>
2023-11-26 16:58:28 -0300

    no

--- tourneyscript.js
+++ tourneyscript.js
@@ -2,12 +2,42 @@
 document.addEventListener('DOMContentLoaded', function () {
     var leftMovie = document.getElementById('movieLeft');
     var rightMovie = document.getElementById('movieRight');
-    
-    // Function to replace movie poster with a new one
-    function replaceMovie(movieElement) {
-        // Logic to replace the movie poster goes here
-    }
 
+        // This function ensures we're always getting the top card.
+        function getTopCard() {
+            const allCards = document.querySelectorAll('.movie-card:not(.burned)');
+            return allCards[allCards.length - 1]; // The last card in the array is the topmost one.
+        }
+    
+        function removeMovie(direction) {
+            const movieElement = getTopCard(); // Always get the top card
+    
+            if (!movieElement) return; // If there's no card, don't do anything
+    
+            // Trigger the flame animation
+            movieElement.classList.add('flame');
+    
+            // Wait for the animation to finish before removing the movie poster
+            setTimeout(function () {
+                movieElement.classList.add('remove');
+                movieElement.classList.add('burned'); // Mark the card as burned so it's not targeted again
+                
+                // After the poster has "burned", remove it and replace with a new one
+                setTimeout(function () {
+                    replaceMovie(movieElement);
+                    movieElement.classList.remove('flame', 'remove');
+                }, 1000); // Corresponds with the duration of the burnAndFade animation
+            }, 3000); // This duration should match the length of your flame GIF or CSS animation
+        }
+    
+        function replaceMovie(movieElement) {
+            // Logic to load a new movie poster goes here
+            // For example, setting new src for the img tag and updating the movie title
+            movieElement.querySelector('img').src = 'new_path_to_movie_poster.jpg';
+            movieElement.querySelector('.movie-title').textContent = 'New Movie Title';
+            movieElement.classList.remove('burned'); // Remove the burned class to reset the card
+        }
+    
     // Function to animate and remove movie poster
     function removeMovie(movieElement, direction) {
         movieElement.classList.add('flame');
