3c6cd1797634ae4ea331a4da85824788610e413c
sutoscience <amy@amysuto.com>
2023-11-27 12:42:05 -0300

    hm

--- tourneyscript.js
+++ tourneyscript.js
@@ -3,19 +3,43 @@ document.addEventListener('DOMContentLoaded', function () {
     var rightMovie = document.getElementById('movieRight');
 
     // This function ensures we're always getting the top card
-    function getTopCard() {
-        const allCards = document.querySelectorAll('.movie-card:not(.burned)');
+    function getTopCard(stackId) {
+        const stack = document.getElementById(stackId);
+        const allCards = stack.querySelectorAll('.movie-card:not(.burned)');
         return allCards[allCards.length - 1]; // The last card in the array is the topmost one
     }
 
-    // Function to remove and replace movie poster
-    function removeMovie(nonSelectedSide) {
-        const movieElement = getTopCard(); // Always get the top card
+    // Modified removeMovie function
+function removeMovie(stackId) {
+    const movieElement = getTopCard(stackId);
 
-        if (!movieElement) return; // If there's no card, don't do anything
+    if (!movieElement) return;
 
-        // Logic to apply the confetti effect to the non-selected poster
-        // ...
+    // Apply the confetti effect and removal logic
+    const confettiContainer = document.getElementById('confettiContainer');
+    const confetti = new ConfettiGenerator({ target: confettiContainer });
+    confetti.render();
+
+    // Explode the card
+    movieElement.classList.add('explode');
+
+    // Replace the movie with the next one in the stack
+    setTimeout(function () {
+        movieElement.classList.add('burned');
+        replaceMovie(movieElement);
+        confetti.clear();
+    }, 1000);
+}
+
+// Event listeners for each stack
+document.getElementById('leftMovieStack').addEventListener('click', function() {
+    removeMovie('rightMovieStack');
+});
+
+document.getElementById('rightMovieStack').addEventListener('click', function() {
+    removeMovie('leftMovieStack');
+});
+
 
         // Wait for the animation to finish before removing the movie poster
         setTimeout(function () {
@@ -32,16 +56,6 @@ document.addEventListener('DOMContentLoaded', function () {
         movieElement.querySelector('.movie-title').textContent = 'New Movie Title';
         movieElement.classList.remove('burned'); // Remove the burned class to reset the card
     }
-
-    // Event listeners for click events on movie posters
-    leftMovie.addEventListener('click', function() {
-        removeMovie('right'); // Remove the right movie
-    });
-
-    rightMovie.addEventListener('click', function() {
-        removeMovie('left'); // Remove the left movie
-    });
-
     // Optional: Add event listener for keydown events for the arrow keys
     document.addEventListener('keydown', function(event) {
         if (event.key === 'ArrowLeft') {
